5850383173dd5face18c5c822efc225b6bab06d3
NamNam <lam2004ha@gmail.com>
2025-03-05 20:20:53 +0700

    Tải lên version 2 của blog

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -9,6 +9,9 @@ def create_app(test_config=None):
     
     from . import db
     db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     from . import blog
     app.register_blueprint(blog.bp)
@@ -22,13 +25,23 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
+        # load the test config if passed in
         app.config.from_mapping(test_config)
 
+
+    
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, this is our lab!'
 
     return app
