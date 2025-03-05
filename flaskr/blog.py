6e493cd0618e04c3b7b25caf616ff6d8a8cacb54
NamNam <lam2004ha@gmail.com>
2025-03-05 20:36:03 +0700

    Tải lên version 5 của dự án

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -11,12 +11,21 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    offset = (page - 1) * per_page
+    
+    # Get total number of posts
+    total_posts = db.execute('SELECT COUNT(*) as count FROM post').fetchone()['count']
+    total_pages = (total_posts + per_page - 1) // per_page
+    
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY created DESC LIMIT ? OFFSET ?',
+        (per_page, offset)
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html', posts=posts, page=page, total_pages=total_pages)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
